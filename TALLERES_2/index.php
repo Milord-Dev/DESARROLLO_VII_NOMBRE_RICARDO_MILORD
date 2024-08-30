94223b9325319001b28dc9893a6da8bee4de662b
ricardo milord <milord.dev05@gmail.com>
2024-08-30 15:49:26 -0500

    Ejercicio práctico: combinación de conceptos aprendidos

--- TALLERES_2/index.php
+++ TALLERES_2/index.php
@@ -1,43 +1,29 @@
 <?php
-// Tu código PHP irá aquí
-$nombre = "Juan";
-$edad = 25;
-$altura = 1.75;
-$esEstudiante = true;
+// Definición de variables
+$nombre = "María";
+$edad = 30;
+$ciudad = "Madrid";
 
-//concatenacion usando operador.
-$presentacion1 = "Hola, mi nombre es " . $nombre . " y tengo de edad " . $edad . " años";
+// Definición de constante
+define("PROFESION", "Ingeniera");
 
-//concatenacion dentro de comillas dobles 
-$presentacion2 = "Hola, mi nombre es $nombre y tengo $edad años";
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Vivo en $ciudad y soy " . PROFESION . ".";
 
-//definicion de una constante 
-define ("SALUDO" , "¡Bienvenido!");
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
 
-//concatenacion con constante 
-$mensaje = SALUDO . " " . $nombre;
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
 
-//impresiones
-echo "Nombre: $nombre <br>";
-echo "Edad: $edad <br>";
-echo "Altura: $altura <br>";
-echo "Es estudiante? ". ($esEstudiante ? "si " : " no " . "<br>");
-echo $presentacion1 . "<br>";
-echo $presentacion2 . "<br>";
-echo $mensaje . "<br>";
-
-//usando echo 
-echo "Hola, Mundo! <br>";
-echo "Mi nombre es $nombre <br>";
-
-//usando print
-print "Tengo $edad años <br>";
-
-//usando printf (permite formateo)
-printf ("Me llamo %s y tengo %d años<br>", $nombre, $edad);
-
-//Usando var_dump (útil para debugging)
+echo "<br>Información de debugging:<br>";
 var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($ciudad);
+echo "<br>";
+var_dump(PROFESION);
 echo "<br>";
 
 ?>
