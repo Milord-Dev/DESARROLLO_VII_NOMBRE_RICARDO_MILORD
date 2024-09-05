c4063b4f03d1782b6836a7dda070bb1819ebede9
ricardo milord <milord.dev05@gmail.com>
2024-09-05 18:21:41 -0500

    Agregar el archivo de Analisis.php

---
+++ PARCIALES/PARCIAL_1/analisis_texto.php
@@ -0,0 +1,10 @@
+<?php 
+include 'utilidades_texto.php';
+//varibles 
+$frase = ["Esta sera una frase para el ejemplo del parcial" . 
+          "Esta es otra frase para que se pueda ver cuantas vocales entre otros" . 
+          "El cielo es azul y no gris, solo es perspectiva."];
+
+          
+
+?>
