7f06e9aad38452b08780b2c09f139834dc24bc43
ricardo milord <milord.dev05@gmail.com>
2024-09-05 20:05:56 -0500

    Funcionalidades bases

--- PARCIALES/PARCIAL_1/utilidades_texto.php
+++ PARCIALES/PARCIAL_1/utilidades_texto.php
@@ -1,3 +1,20 @@
 <?php 
+//Funcione de contar palabras
+function contar_palabras($texto){
+    $palbras = str_word_count($texto);
+    return $palbras;
+};
 
+//Funcion de contar vocales
+function contar_vocales($texto){
+    $vocales = ["a,e,i,o,u"];
+    
+};
+
+//Funcion de invertir palabras
+function invertir_palabras($texto){
+        $palabras = explode(' ', $texto);
+        $palabras_invertidas = array_reverse($palabras);
+        return implode(' ', $palabras_invertidas);
+};
 ?>
