6315e1ea140b5c1ab652ff20f2fc9edf5002625f
ricardo milord <milord.dev05@gmail.com>
2024-10-24 19:01:45 -0500

    verificacion de usuario y obtener la tarea

--- PARCIALES/PARCIAL_3/dashboard.php
+++ PARCIALES/PARCIAL_3/dashboard.php
@@ -1,6 +1,12 @@
 <?php
+session_start();
 
-
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.php");
+    exit();
+}
+//Obtener las tareas del usuario
+$tareas = $_SESSION['tareas'] ?? [];
 ?>
 
 <!DOCTYPE html>
